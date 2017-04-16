b6bdc73fed092af1e6455e5603b4128f6e7bb646
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-04-16 18:01:13 +0200

    Data input files

---
+++ src/luciola/data/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Keeps the two CVS files with the input data (answers from workers):
+ * answerList_photinus_data.csv (data from the experiment executed in year 2015)
+ * answerList_firefly_data.csv (data from the experiment executed in year 2014)
+ */
+/**
+ * @author Christian Adriano
+ *
+ */
+package luciola.data;
