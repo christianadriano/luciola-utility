e2b1b8942ad312076892d2866a0dc28eb8298d50
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-04-17 23:28:40 +0200

    Testing the sampler

---
+++ src/luciola/test/AnswerSamplerTest.java
@@ -0,0 +1,37 @@
+package luciola.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import luciola.data.wrangling.AnswerSampler;
+import luciola.data.wrangling.AnswerSampler.TrainingTestingPair;
+
+public class AnswerSamplerTest {
+
+	@Test
+	public void test() {
+		AnswerSampler sampler = new AnswerSampler();
+		
+		int trainingSize = new Double(2579).intValue();
+		ArrayList<TrainingTestingPair> pairList = sampler.generateTrainingTestingSets(trainingSize, 2);
+	
+		TrainingTestingPair pair_1 = pairList.get(0);
+		System.out.println(pair_1.testingMap.size());
+		
+		TrainingTestingPair pair_2 = pairList.get(1);
+		System.out.println(pair_2.trainingMap.size());	
+	
+		Iterator<String> keys = pair_1.testingMap.keySet().iterator();
+		while(keys.hasNext()){
+			String key = keys.next();
+			Assert.assertFalse("Pair 2 contains:"+key,pair_2.testingMap.containsKey(key));
+		}
+	
+	}
+
+}
